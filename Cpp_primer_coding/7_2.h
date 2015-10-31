8763eed3a936a22e544c16ef53c857073637689c
blueyi <blueyiniu@qq.com>
2015-10-31 12:58:48 +0800

    back

--- Cpp_primer_coding/7_2.h
+++ Cpp_primer_coding/7_2.h
@@ -7,13 +7,23 @@
 #ifndef 7_2CPP_H
 #define 7_2CPP_H
 
+#include <string>
+
 using namespace std;
 struct Sales_data {
     std::string isbn(); 
+    Sales_data& combine(const Sales_data &rhs);
+
     std::string bookNo;
     unsigned units_sold = 0;
     double revenue = 0.0;
 };
+Sales_data& Sales_data::combine(const Sales_data &rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
 int main()
 {
     Sales_data total;
